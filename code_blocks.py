e756f34e5fc65d46321caacdeeb46c4898a80a98
volkir31 <egor_mixajlichenko@mial.ru>
2020-12-25 20:07:53 +0300

    refactoring

--- code_blocks.py
+++ code_blocks.py
@@ -1,28 +1,42 @@
 token = 12314
+count_condition = 2
 start = f'''import telebot
+
 
 token = '{token}'
 bot = telebot.TeleBot(token=token)
 
 
 '''
-mess = 'привет'
-response = 'Привет. Чем я могу помочь?'
+mess = ['привет', 'какое расписание?']
+response = ['Привет. Чем я могу помочь?', 'ПН-ПТ с 8:00 до 20:00']
 
-condition = f"""if message.text == '{mess}':
+
+def make_condition(message, response):
+    condition = f"""if message.text == '{message}'.upper():
         bot.send_message(message.from_user.id, '{response}')
-"""
-text_response = f"""@bot.message_handler(content_types=['text'])
+    """
+    return condition
+
+condition = ''
+for i in range(count_condition):
+    me = mess[i]
+    res = response[i]
+    condition += make_condition(me, res)
+
+body = f"""@bot.message_handler(content_types=['text'])
 def text_response(message):
     {condition}
-
-
+    
 """
 
 
-# bot.polling(none_stop=True, interval=0)
+end = '''bot.polling(none_stop=True, interval=0)
+'''
+
 
 with open('main.py', 'a+') as f:
     f.write(start)
-    f.write(text_response)
+    f.write(body)
+    f.write(end)
 
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import telebot
 
+
 token = '12314'
 bot = telebot.TeleBot(token=token)
 
 
 @bot.message_handler(content_types=['text'])
 def text_response(message):
-    if message.text == 'привет':
+    if message.text == 'привет'.upper():
         bot.send_message(message.from_user.id, 'Привет. Чем я могу помочь?')
-
-
-
+    if message.text == 'какое расписание?'.upper():
+        bot.send_message(message.from_user.id, 'ПН-ПТ с 8:00 до 20:00')
+    
+    
+bot.polling(none_stop=True, interval=0)
