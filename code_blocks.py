c612c0a0aaf4958c96887100853743e0e4bf66ee
Yarco62 <nero020304@gmail.com>
2021-01-04 17:13:39 +0700

    base core
    Сделана обработка простейших инструкций

--- code_blocks.py
+++ code_blocks.py
@@ -1,4 +1,5 @@
-token = 12314
+token = "925344997:AAGWfYn_GkgWBd1ViHdp6KcYmhG51yKSjJQ"
+
 
 def make_conditions(request, response):
     condition = f'''
@@ -6,7 +7,12 @@ def make_conditions(request, response):
         bot.send_message(message.from_user.id, '{response}')'''
     return condition
 
-def text_response(conditions):
+
+def text_response(text_res):
+    conditions = ''
+    for cond in text_res:
+        conditions += make_conditions(*cond)
+
     body = f"""
 @bot.message_handler(content_types=['text'])
 def text_response(message):
@@ -16,8 +22,18 @@ def text_response(message):
     return body
 
 
-start = f'''import telebot
+def start_message(message):
+    greetings = f'''
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "{message}")
+    '''
+    return greetings
 
+
+start = f'''# -*- coding: cp1251 -*-
+
+import telebot
 
 token = '{token}'
 bot = telebot.TeleBot(token=token)
@@ -27,12 +43,29 @@ bot = telebot.TeleBot(token=token)
 end = '''
 bot.polling(none_stop=True, interval=0)
 '''
+text_res = []
 
-with open('main.py', 'a+') as f:
-    f.write(start)
-    text_res = [('hello', 'hello, my friend'),('hi', 'hi, bitch')]
-    conditions = ''
-    for cond in text_res:
-        conditions += make_conditions(*cond)
-    f.write(text_response(conditions))
-    f.write(end)
+with open('Instructions Example.txt', 'r') as instructions:
+    with open('main.py', 'w+') as f:
+        for line in instructions:
+            print(line)
+            if line.split()[0] == 'token':
+                token = line.strip()[1]
+            elif line.split()[0] == 'start':
+                print('start')
+                f.write(start)
+            elif line.split()[0] == 'start_message':
+                f.write(start_message(line.strip().split('start_message')[1]))
+            elif line.split()[0] == 'text_response':
+                text_res.append((line.split('\"\"')[1], line.split('\"\"')[3]))
+            elif line.split()[0] == 'end':
+                pass
+        f.write(text_response(text_res))
+        f.write(end)
+            #with open('main.py', 'w+') as f:
+               # f.write(start)
+               # f.write(start_message('Спасибо что запустил меня бро'))
+              #  text_res = [('hello', 'hello, my friend'), ('hi', 'hi, bitch'), ('1+1', '2')]
+
+              #  f.write(text_response(text_res))
+              #  f.write(end)
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+# -*- coding: cp1251 -*-
+
 import telebot
 
-
-token = '1234'
+token = '925344997:AAGWfYn_GkgWBd1ViHdp6KcYmhG51yKSjJQ'
 bot = telebot.TeleBot(token=token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, " Hello, thanks for starting me")
 
 
 @bot.message_handler(content_types=['text'])
 def text_response(message):
-    
-    if message.text.upper() == 'hello'.upper():
-        bot.send_message(message.from_user.id, 'hello, my friend')
-    if message.text.upper() == 'hi'.upper():
-        bot.send_message(message.from_user.id, 'hi, bitch')
+    if message.text.upper() == '1 + 0'.upper():
+        bot.send_message(message.from_user.id, '10')
 
-    
+
 bot.polling(none_stop=True, interval=0)
